24762f9edf17ae3f0cac8623f67e2bf419cc1bda
Reuben Posthuma <reuben.posthuma@gmail.com>
2024-03-22 17:00:54 +1300

    made sure we don't overwrite .env

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+cp example.env .env
 cp printer-client.service /lib/systemd/system/printer-client.service
 systemctl enable printer-client.service
 systemctl daemon-reload
+systemctl start printer-client.service
