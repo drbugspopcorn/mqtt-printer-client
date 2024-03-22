a097706fc85d37f8f4dcc760aad9835b28891d5c
Reuben Posthuma <reuben.posthuma@gmail.com>
2024-03-22 16:55:00 +1300

    Fixed typo

--- printer_handler_cups.py
+++ printer_handler_cups.py
@@ -15,7 +15,7 @@ class PrinterHandler():
         printers = self.conn.getPrinters()
         local_printer_list = []
         for k in printers.keys():
-            if not printers[k]['printer_type'] & 0b10:      # docs - https://www.cups.org/doc/spec-ipp.html printer type enum
+            if not printers[k]['printer-type'] & 0b10:      # docs - https://www.cups.org/doc/spec-ipp.html printer type enum
                 local_printer_list.append(k)
         return local_printer_list
 
